41852f0e8fd18b22df02c82ca769962720e0dc59
Adedoyin Kassem <doyin.kassem@gmail.com>
2026-01-07 16:29:23 -0500

    build: add compiled assets and package-lock.json

---
+++ assets/js/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'cc8e569abda537a3853c');
---
+++ assets/js/block-policy-table.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'efd2c5c4e35641f87cfb');
